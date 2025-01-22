c2811f667ef7957b4bb6dd0e7ee7b76da1d6da12
mikoslaf <mikolaj2003wojcik@gmail.com>
2025-01-22 15:31:39 +0100

    Add poetry and implement basic webhook functionality

--- discord_module.py
+++ discord_module.py
@@ -1,25 +1,27 @@
-from discord_webhook import DiscordWebhook, DiscordEmbed
+import requests
 
 class discord_Webhooks:
-    _webhook : DiscordWebhook
-    #allowed_mentions: list[dict[list[str]]]
-    deafult_color: str
+    
+    def __init__(self, webhook_link: str) -> None:
+        self.webhook_link = webhook_link
 
-    def __init__(self, webhook_link: str, user_allowed_mentions: list[str] = [], deafult_color: str = "03b2f8") -> None:
-        allowed_mentions = {
-            "parse": ["everyone", "here"],
-            "users": user_allowed_mentions
-        }
-        self.deafult_color = deafult_color
-        self._webhook = DiscordWebhook(webhook_link, allowed_mentions=allowed_mentions)
+    def send_message(self, content: str) -> int:
+        return requests.post(self.webhook_link, json = {"content": content})
+    
+    def get_webhook_link(self) -> str:
+        return requests.get(self.webhook_link)
 
-    def send_discord_simple_message(self, content: str) -> int:
-        self._webhook.set_content(content=content) 
-        response = self._webhook.execute()
-        return response.status_code
+    def send_basic_embed(self, title: str, description: str, color: str) -> int:
 
-    def send_discord_embed_message(self, title: str, description: str, color: str) -> int:
-        embed = DiscordEmbed(title=title, description=description, color=color)
-        self._webhook.add_embed(embed=embed)
-        response = self._webhook.execute()
-        return response.status_code
+        embed_data = {
+            "title": title, 
+            "description": description, 
+            "color": color 
+            }
+
+        return requests.post(self.webhook_link, json = {"embeds": [embed_data]})
+
+
+if __name__ == "__main__":
+    pass
+    
